b2c7fcb760dd91569697c997cc18c84ce890272f
Haripriya24071 <haripriyar891@gmail.com>
2026-09-22 21:57:06 +0530

    feat(models): export all SQLAlchemy models in models package init for Alembic detection

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,1 +1,16 @@
-# ApiRadar - backend/models/__init__.py
+from models.user import User
+from models.api_catalog import APICatalog
+from models.stack_profile import StackProfile, WatchedAPI
+from models.change_event import ChangeEvent
+from models.openapi_snapshot import OpenAPISnapshot
+from models.notification import Notification
+
+__all__ = [
+    "User",
+    "APICatalog",
+    "StackProfile",
+    "WatchedAPI",
+    "ChangeEvent",
+    "OpenAPISnapshot",
+    "Notification",
+]
