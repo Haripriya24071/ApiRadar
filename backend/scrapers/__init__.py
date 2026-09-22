defd653a149f0ac2a295dcd783ac5e6a03c4414a
Haripriya24071 <haripriyar891@gmail.com>
2026-09-22 22:20:37 +0530

    feat(scrapers): export GitHubReleaseScraper in package init

--- backend/scrapers/__init__.py
+++ backend/scrapers/__init__.py
@@ -1,4 +1,5 @@
 from scrapers.base import BaseScraper, RawChange
 from scrapers.rss_scraper import RSSScraper
+from scrapers.github_scraper import GitHubReleaseScraper
 
-__all__ = ["BaseScraper", "RawChange", "RSSScraper"]
+__all__ = ["BaseScraper", "RawChange", "RSSScraper", "GitHubReleaseScraper"]
